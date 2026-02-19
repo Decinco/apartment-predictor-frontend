2723128efd438c8f928789a8c6ced1804671f0e3
Daniel Mugueta <103062948+Decinco@users.noreply.github.com>
2026-02-19 14:11:48 +0100

    detailview colored

--- apartment-predictor-frontend/src/components/ApartmentDetail.tsx
+++ apartment-predictor-frontend/src/components/ApartmentDetail.tsx
@@ -1,6 +1,7 @@
 import { IconAirConditioning, IconBath, IconBed, IconHotelService, IconParking, IconSofa, IconSquareAsterisk, IconStack, IconStairsDown, IconTemperaturePlus, type Icon } from "@tabler/icons-react"
 import type { Apartment } from "../data/Apartment"
 import { separateNumbers } from "../utils/utils"
+import Button from "./ui/Button"
 
 
 
@@ -8,93 +9,37 @@ export default function ApartmentDetail({ apartment, onStartEdit, onReturn, onDe
     return (
         <div className="flex flex-col items-center antialiased self-center pb-5">
             <div className="px-5">
-                <h3 className="text-5xl font-bold italic pt-5">{apartment.name}</h3>
+                <h3 className="text-5xl text-(--title) font-bold italic pt-5">{apartment.name}</h3>
                 <p className="text-3xl font-medium text-left self-baseline pb-5">{separateNumbers(apartment.price)} €</p>
                 <div className="flex flex-col gap-3 pb-5">
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconStack className="p-1" size={48}></IconStack>
-                        </div>
-                        <span className="text-xl"><span className="text-yellow-600 font-bold">Stories: </span>{apartment.stories}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconSquareAsterisk className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl"><span className="text-yellow-600 font-bold">Area: </span>{apartment.area} m²</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconBed className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl"><span className="text-yellow-600 font-bold">Bedrooms: </span>{apartment.bedrooms}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconBath className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl"><span className="text-yellow-600 font-bold">Bathrooms: </span>{apartment.bathrooms}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconAirConditioning className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl">{apartment.airconditioning ? <span className="text-yellow-600 font-bold">Has air conditioning</span> : <span className="font-bold">Doesn't have air conditioning</span>}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconStairsDown className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl">{apartment.basement ? <span className="text-yellow-600 font-bold">Has a basement</span> : <span className="font-bold">Doesn't have a basement</span>}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconHotelService className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl">{apartment.guestroom ? <span className="text-yellow-600 font-bold">Has a guest room</span> : <span className="font-bold">Doesn't have a guest room</span>}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconTemperaturePlus className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl">{apartment.hotwaterheating ? <span className="text-yellow-600 font-bold">Has water heating</span> : <span className="font-bold">Doesn't have water heating</span>}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconParking className="p-1" size={48} />
-                        </div>
-                        <span className="text-xl font-bold">{apartment.parking ? <span className="text-yellow-600 font-bold">Has parking</span> : <span className="font-bold">Doesn't have parking</span>}</span>
-                    </div>
-                    <div className="flex flex-row gap-3 items-center">
-                        <div className="bg-linear-to-br from-yellow-800 to-yellow-600 rounded-xl">
-                            <IconSofa className="p-1" size={48}></IconSofa>
-                        </div>
-                        <span className="text-xl"><span className="text-yellow-600 font-bold">Furnishing status: </span>{apartment.furnishingstatus}</span>
-                    </div>
+                    <FieldStatistics field={apartment.stories} fieldDisplay="Stories" icon={IconStack} />
+                    <FieldStatistics field={apartment.area} fieldDisplay="Area" icon={IconSquareAsterisk} />
+                    <FieldStatistics field={apartment.bedrooms} fieldDisplay="Bedrooms" icon={IconBed} />
+                    <FieldStatistics field={apartment.bathrooms} fieldDisplay="Bathrooms" icon={IconBath} />
+                    <FieldStatistics field={apartment.airconditioning} fieldDisplay="air conditioning" icon={IconAirConditioning} />
+                    <FieldStatistics field={apartment.basement} fieldDisplay="a basement" icon={IconStairsDown} />
+                    <FieldStatistics field={apartment.guestroom} fieldDisplay="a guest room" icon={IconHotelService} />
+                    <FieldStatistics field={apartment.hotwaterheating} fieldDisplay="water heating" icon={IconTemperaturePlus} />
+                    <FieldStatistics field={apartment.parking} fieldDisplay="parking" icon={IconParking} />
+                    <FieldStatistics field={apartment.furnishingstatus} fieldDisplay="Furnishing Status" icon={IconSofa} />
                 </div>
             </div>
             <div>
             </div>
             <div className="flex flex-row gap-4">
-                <button onClick={onReturn} className="inset-ring-4 rounded-lg hover:cursor-pointer py-2 px-6 inset-ring-yellow-800 font-bold hover:inset-ring-0 hover:bg-linear-to-br hover:from-yellow-800 hover:to-yellow-600">
-                    Return to List
-                </button>
-                <button onClick={onStartEdit} className="inset-ring-4 rounded-lg hover:cursor-pointer py-2 px-6 inset-ring-yellow-800 font-bold hover:inset-ring-0 hover:bg-linear-to-br hover:from-yellow-800 hover:to-yellow-600">
-                    Edit
-                </button>
-                <button onClick={() => onDelete(apartment.id, apartment.name)} className="inset-ring-4 rounded-lg hover:cursor-pointer py-2 px-6 inset-ring-red-800 font-bold hover:inset-ring-0 hover:bg-linear-to-br hover:from-red-800 hover:to-red-600">
-                    Delete
-                </button>
+                <Button text="Return to List" onClick={onReturn} />
+                <Button text="Edit" onClick={onStartEdit} />
+                <Button text="Delete" type="destructive" onClick={() => onDelete(apartment.id, apartment.name)} />
             </div>
         </div>
     )
 }
 
-function FieldStatistics({field, fieldDisplay, icon}: {field: any, fieldDisplay?: string, icon: Icon}) {
-    <div className="flex flex-row gap-3 items-center">
+function FieldStatistics({ field, fieldDisplay, icon: IconComponent }: { field: any, fieldDisplay?: string, icon: Icon }) {
+    return (<div className="flex flex-row gap-3 items-center">
         <div className="bg-linear-to-br from-(--accent-start) to-(--accent-end) rounded-xl">
-            {icon}
+            <IconComponent className="p-1" size={48} />
         </div>
-        {typeof field == "boolean" ? <span className="text-xl">{field ? <span className="text-(--title)">Has {fieldDisplay ?? field}</span> : <span className="font-bold">Doesn't have {fieldDisplay ?? field}</span>}</span> : <span className="text-xl"><span className="text-(--title) font-bold">{fieldDisplay ?? field}: </span>{field}</span>}
-    </div>
+        {typeof field == "boolean" ? <span className="text-xl">{field ? <span className="font-bold text-(--title)">Has {fieldDisplay ?? field}</span> : <span className="">Doesn't have {fieldDisplay ?? field}</span>}</span> : <span className="text-xl"><span className="text-(--title) font-bold">{fieldDisplay ?? field}: </span>{field}</span>}
+    </div>)
 }
