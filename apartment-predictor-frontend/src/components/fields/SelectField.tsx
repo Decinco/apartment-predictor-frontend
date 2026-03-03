2da2c86bf9132665b3c34f8cb8a064dcf000123e
Decinco <danielmug.dm@gmail.com>
2026-03-03 10:39:59 +0100

    final light theme touches

--- apartment-predictor-frontend/src/components/fields/SelectField.tsx
+++ apartment-predictor-frontend/src/components/fields/SelectField.tsx
@@ -10,10 +10,10 @@ interface SelectFieldProps {
 export default function SelectField({ label, value, options, onChange }: SelectFieldProps) {
     return (
         <div className="flex flex-col md:flex-row gap-4 items-start md:items-center">
-            <label className="text-xl text-yellow-600 font-bold capitalize">
+            <label className="text-xl text-(--title) font-bold capitalize">
                 {label}
             </label>
-            <select className="appearance-none w-full px-4 py-2 text-lg text-white font-normal ring-yellow-600 ring-2 rounded-lg bg-yellow-950/60 hover:bg-yellow-950 focus:bg-yellow-950 disabled:bg-yellow-900/20 disabled:text-gray-300 cursor-pointer"
+            <select className="appearance-none w-full px-4 py-2 text-lg text-(--text) font-normal ring-(--accent-start) ring-2 rounded-lg bg-(--field-bg)/60 hover:bg-(--field-bg) focus:bg-(--field-bg) disabled:bg-(--field-bg)/20 disabled:text-(--text)/40 cursor-pointer transition"
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
             >
--- apartment-predictor-frontend/src/data/Themes.ts
+++ apartment-predictor-frontend/src/data/Themes.ts
@@ -23,7 +23,7 @@ export const Themes: ApartmentApplicationTheme[] = [
       "--accent-hover-start":               cssVar("--color-yellow-600"),
       "--accent-hover-end":                 cssVar("--color-yellow-600"),
       "--border-regular":                   cssVar("--color-yellow-700"),
-      "--accent-hover-destructive-start":   cssVar("--color-red-800"),
+      "--accent-hover-destructive-start":   cssVar("--color-red-600"),
       "--accent-hover-destructive-end":     cssVar("--color-red-600"),
       "--border-destructive":               cssVar("--color-red-700"),
       "--field-border":                     cssVar("--color-yellow-600"),
@@ -44,12 +44,12 @@ export const Themes: ApartmentApplicationTheme[] = [
       "--accent-hover-start":               cssVar("--color-yellow-600"),
       "--accent-hover-end":                 cssVar("--color-yellow-600"),
       "--border-regular":                   cssVar("--color-yellow-500"),
-      "--accent-hover-destructive-start":   cssVar("--color-red-800"),
-      "--accent-hover-destructive-end":     cssVar("--color-red-600"),
+      "--accent-hover-destructive-start":   cssVar("--color-red-500"),
+      "--accent-hover-destructive-end":     cssVar("--color-red-500"),
       "--border-destructive":               cssVar("--color-red-500"),
-      "--field-border":                     cssVar("--color-yellow-600"),
-      "--field-bg":                         cssVar("--color-yellow-950"),
-      "--checkmark":                        cssVar("--color-yellow-600"),
+      "--field-border":                     cssVar("--color-yellow-400"),
+      "--field-bg":                         cssVar("--color-yellow-50"),
+      "--checkmark":                        cssVar("--color-yellow-400"),
     }
   }
 ]
