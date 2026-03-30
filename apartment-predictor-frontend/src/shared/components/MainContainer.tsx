c43931ccd2abd28a8da5a5f42a89a24bef1ff737
Daniel Mugueta <danielmug.dm@gmail.com>
2026-03-30 14:06:11 +0200

    font ^-^

--- apartment-predictor-frontend/src/shared/components/MainContainer.tsx
+++ apartment-predictor-frontend/src/shared/components/MainContainer.tsx
@@ -11,7 +11,7 @@ export default function MainContainer({children}: {children: ReactNode | ReactNo
             style={theme.vars as React.CSSProperties}
         >
             <div className="relative flex flex-row justify-between md:justify-center rounded-xl bg-(--text)/10 p-3 items-center">
-                <h2 className="cursor-default text-(--title) decoration-2 italic decoration-double underline-offset-4 decoration-(--text) underline font-bold text-4xl text-center self-center">Apartment Predictor</h2>
+                <h2 className="cursor-default text-(--title) decoration-2 italic decoration-double underline-offset-4 decoration-(--text) underline font-black text-4xl text-center self-center">Apartment Predictor</h2>
                 <div className="md:absolute md:right-3 md:top-1/2 md:-translate-y-1/2">
                     <ThemeChanger></ThemeChanger>
                 </div>
